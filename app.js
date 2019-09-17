65d2b2769daf959af212bc1c48aa2cddaea5a64e
Jacky <jacky.ng9188@gmail.com>
2019-09-17 15:47:19 -0400

    StartGame method

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 var app = new Vue ({
     el: '#app',
+
     data: {
         playerHealth: 100,
-        monsterHealth: 100
+        monsterHealth: 100,
+        gameIsRunning: false
+    },
+
+    methods: {
+        startGame: function() {
+            this.gameIsRunning = true;
+            this.playerHealth = 100;
+            this.monsterHealth = 100;
+        }
     }
 })
