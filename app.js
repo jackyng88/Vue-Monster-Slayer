45e4baedb1f8e44218a3a58f19605cd32a8404c6
Jacky <jacky.ng9188@gmail.com>
2019-09-17 22:34:54 -0400

    Implemented attack, specialattack, and giveup functions.

--- app.js
+++ app.js
@@ -12,6 +12,62 @@ var app = new Vue ({
             this.gameIsRunning = true;
             this.playerHealth = 100;
             this.monsterHealth = 100;
+        },
+        attack: function() {
+            this.monsterHealth -= this.calculateDamage(3, 10);
+            if (this.checkWin()) {
+                return;
+            }
+
+            this.monsterAttack();
+        },
+        monsterAttack: function() {
+            this.playerHealth -= this.calculateDamage(5, 12);
+            this.checkWin();
+        },
+        specialAttack: function() {
+            this.monsterHealth -= this.calculateDamage(10, 20);
+            if (this.checkWin()) {
+                return;
+            }
+            this.monsterAttack();
+        },
+        heal: function() {
+            if (this.playerHealth <= 90) {
+                this.playerHealth += 10;
+            }
+            else {
+                this.playerHealth = 100;
+            }
+            this.monsterAttack();
+        },
+        giveUp: function() {
+            this.gameIsRunning = false;
+        },
+        calculateDamage: function(min, max) {
+            return Math.max(Math.floor(Math.random() * max) + 1, min);
+        },
+        checkWin: function() {
+            // Check to see game is over by ways of monsterHealth.
+            if (this.monsterHealth <= 0) {
+                if (confirm('You have won. Start new game?')) {
+                    this.startGame();
+                }
+                else {
+                    this.gameIsRunning = false;
+                }
+                return true;
+            }
+            else if (this.playerHealth <= 0) {
+                if (confirm('You have lost. Start new game?')) {
+                    this.startGame();
+                }
+                else {
+                    this.gameIsRunning = false;
+                }
+                return true;
+            }
+            return false;
         }
     }
 })
